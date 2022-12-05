2596565b661fe65e3ea0026dd36123e5f24f801f
kosuke-samurai <103375028+kosuke-samurai@users.noreply.github.com>
2022-12-05 16:34:56 +0900

    試しにクリックイベントを書いてみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,27 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
+import { useCallback } from 'react'
+
+// const handleClick = (e, foo) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo);
+// }
+
 
 
 export default function Home() {
+
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +33,14 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <a
+        href='/about'
+        onClick={handleClick}
+      >
+
+        ボタン
+      </a>
 
       <Main page='index' />
 
