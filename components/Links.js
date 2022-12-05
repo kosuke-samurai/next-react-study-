07eed832e23292c43ed517dfa8cfb06027c83c0a
kosuke-samurai <103375028+kosuke-samurai@users.noreply.github.com>
2022-12-05 13:38:09 +0900

    LInksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,16 +1,44 @@
 import Image from 'next/image'
 import classes from './Links.module.css'
 
+const ITEMS = [
+    {
+        href: 'https://nextjs.org/docs',
+        title: 'Documentation \u2192',
+        description: 'Find in-depth information about Next.js features and API.'
+    },
+    {
+        href: 'https://nextjs.org/learn',
+        title: 'Learn \u2192',
+        description: 'Learn about Next.js in an interactive course with quizzes!'
+    },
+    {
+        href: 'https://github.com/vercel/next.js/tree/canary/examples',
+        title: 'Examples →',
+        description: 'Discover and deploy boilerplate example Next.js projects.'
+    },
+    {
+        href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+        title: 'Deploy →',
+        description: 'Instantly deploy your Next.js site to a public URL with Vercel.'
+    },
+]
 
 export function Links() {
+
     return (
         <div className={classes.grid}>
-            <a href="https://nextjs.org/docs" className={classes.card}>
-                <h2>Documentation &rarr;</h2>
-                <p className={classes.description}>Find in-depth information about Next.js features and API.</p>
-            </a>
 
-            <a href="https://nextjs.org/learn" className={classes.card}>
+            {ITEMS.map(item => {
+                return (
+                    <a key={item.href} href={item.href} className={classes.card}>
+                        <h2>{item.title}</h2>
+                        <p className={classes.description}>{item.description}</p>
+                    </a>
+                )
+            })}
+
+            {/* <a href="https://nextjs.org/learn" className={classes.card}>
                 <h2>Learn &rarr;</h2>
                 <p className={classes.description}>Learn about Next.js in an interactive course with quizzes!</p>
             </a>
@@ -33,8 +61,8 @@ export function Links() {
                 <p className={classes.description}>
                     Instantly deploy your Next.js site to a public URL with Vercel.
                 </p>
-            </a>
+            </a> */}
         </div>
 
-    )
+    );
 }
