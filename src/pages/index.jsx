cf9515ad2ee796540995cdcf5cd255dcc817891c
kosuke-samurai <103375028+kosuke-samurai@users.noreply.github.com>
2022-12-05 22:31:15 +0900

    useState(カウンターを設置してカウントアップ）

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 
 // const handleClick = (e, foo) => {
@@ -16,13 +16,14 @@ import Link from 'next/link'
 
 
 export default function Home() {
+  const [count, setCount] = useState(1);
 
-  const foo = 1
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+
+  };
 
   useEffect(() => {
 
@@ -37,6 +38,7 @@ export default function Home() {
   }, []);
 
 
+
   return (
     <div className={styles.container}>
       <Head>
@@ -46,12 +48,13 @@ export default function Home() {
 
       <Header />
 
-      <Link
+      <h1>{count}</h1>
+      <button
         href='/about'
         onClick={handleClick}
       >
         ボタン
-      </Link>
+      </button>
 
       <Main page='index' />
 
